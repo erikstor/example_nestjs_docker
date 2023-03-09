cbb6be59f634a7b8171f1c7736dbfc7ea3eb3e78
Erik Stor <erik.stor.franco@gmail.com>
2023-03-09 11:56:50 -0500

    fixed

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     const configService = app.get(ConfigService);
     const port = configService.get<number>('port');
-    await app.listen(port, () => console.log(`Running on port ${port}`));
+    await app.listen(port||3000, () => console.log(`Running on port ${port}`));
 }
 
 bootstrap();
